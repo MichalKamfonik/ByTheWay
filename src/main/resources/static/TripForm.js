33a2d4fc9fd10e87659d56d264c50dbba114ccc3
MichalKamfonik <michal.kamfonik@gmail.com>
2021-02-17 22:48:10 +0100

    Development of JS form

--- src/main/resources/static/TripForm.js
+++ src/main/resources/static/TripForm.js
@@ -1,22 +1,77 @@
 const apihost = window.location.origin;
 const csrfToken = document.querySelector("#_CSRF_").value;
+const activities = [];
+const DAY = 24 * 60;
 
+class Activity {
+    constructor(place, duration, arrival, departure, number, description) {
+        this.place = place;
+        this.duration = duration;
+        this.arrival = arrival;
+        this.departure = departure;
+        this.number = number;
+        this.description = description;
+    }
+}
 document.addEventListener('DOMContentLoaded', function () {
+    const x = document.getElementsByClassName("tab");
+    x[0].style.display = "block";
+
     const tripForm = document.querySelector('form');
 
     tripForm.addEventListener('submit', async function (e) {
-        e.preventDefault()
-        let placeOrigin      = await apiGetPlace(this.origin.value     ).then(place => place);
-        let placeDestination = await apiGetPlace(this.destination.value).then(place => place);
+        e.preventDefault();
+        x[0].style.display = "none";    // hide current tab
+        x[1].style.display = "block";   // show next tab
 
-        let travelTimeThere = await apiGetRouteTime(placeOrigin, placeDestination).then(time => time);
-        let travelTimeBack  = await apiGetRouteTime(placeDestination, placeOrigin).then(time => time);
+        const departureTime = this.departure.value.split(":");
+        const comebackTime = this.comeback.value.split(":");
 
-        let alongRouteThere = await apiGetAlongRoute(placeOrigin, placeDestination, travelTimeThere);
-        let alongRouteBack  = await apiGetAlongRoute(placeDestination, placeOrigin, travelTimeBack );
+        const tripStartInMinutes = departureTime[0] * 60 + parseInt(departureTime[1]);
+        const tripEndInMinutes = comebackTime[0] * 60 + parseInt(comebackTime[1]);
+        const tripDuration = (this.days.value - 1) * DAY + parseInt(tripEndInMinutes) - tripStartInMinutes;
 
-        console.log(alongRouteThere);
-        console.log(alongRouteBack);
+        const placeOrigin = await apiGetPlace(this.origin.value).then(place => place);
+        const placeDestination = await apiGetPlace(this.destination.value).then(place => place);
+
+        const travelTimeThere = (await apiGetRouteTime(placeOrigin, placeDestination).then(time => time) / 60).toFixed(0);
+        const travelTimeBack = (await apiGetRouteTime(placeDestination, placeOrigin).then(time => time) / 60).toFixed(0);
+
+        const start = new Activity(
+            placeOrigin,
+            0,
+            this.departure.value,
+            this.departure.value,
+            0,
+            "ORIGIN"
+        );
+        const destination = new Activity(
+            placeDestination,
+            (tripDuration - travelTimeThere - travelTimeBack).toFixed(0),
+            timeAfterMinutes(this.departure.value, travelTimeThere),
+            timeBeforeMinutes(this.comeback.value, travelTimeBack),
+            1,
+            "DESTINATION"
+        );
+        const end = new Activity(
+            placeOrigin,
+            0,
+            this.comeback.value,
+            this.comeback.value,
+            2,
+            "ORIGIN"
+        );
+
+        activities.push(start);
+        activities.push(destination);
+        activities.push(end);
+
+        renderActivities(activities);
+
+        const alongRouteThere = await apiGetAlongRoute(placeOrigin, placeDestination, travelTimeThere * 60);
+        const alongRouteBack = await apiGetAlongRoute(placeDestination, placeOrigin, travelTimeBack * 60);
+
+        renderAlongRoute(alongRouteThere);
     });
 });
 
@@ -38,6 +93,41 @@ function apiGetPlace(query) {
             return resp.json();
         }
     )
+}
+
+function timeAfterMinutes(time, minutes) {
+    const timeH = parseInt(time.split(":")[0]);
+    const timeM = parseInt(time.split(":")[1]);
+
+    const minutesH = (minutes / (60)).toFixed(0) % 24;
+    const minutesM = minutes % 60;
+
+    const summaryM = timeM + minutesM;
+    const summaryH = timeH + minutesH + (summaryM / 60).toFixed(0);
+
+    return String(summaryH % 24).padStart(2, '0') + ":" + String(summaryM % 60).padStart(2, '0');
+}
+
+function timeBeforeMinutes(time, minutes) {
+    const timeH = parseInt(time.split(":")[0]);
+    const timeM = parseInt(time.split(":")[1]);
+
+    const minutesH = (minutes / (60)).toFixed(0) % 24;
+    const minutesM = minutes % 60;
+
+    const summaryM = timeM - minutesM;
+    const summaryH = timeH - minutesH - (summaryM < 0 ? 1 : 0);
+
+    return String((24 + summaryH) % 24).padStart(2, '0') + ":" + String((60 + summaryM) % 60).padStart(2, '0');
+}
+
+function minutesBetweenTimes(start, end) {
+    const startH = parseInt(start.split(":")[0]);
+    const startM = parseInt(start.split(":")[1]);
+
+    const endH = parseInt(end.split(":")[0]);
+    const endM = parseInt(end.split(":")[1]);
+    return ((endH * 60 + endM) - (startH * 60 + startM) + DAY) % (DAY);
 }
 
 function apiGetRouteTime(origin, destination) {
@@ -62,7 +152,7 @@ function apiGetRouteTime(origin, destination) {
 
 function apiGetAlongRoute(origin, destination, travelTime) {
     return fetch(
-        apihost + '/rest/find-along-route/'+travelTime,
+        apihost + '/rest/find-along-route/' + travelTime,
         {
             headers: {
                 'Content-Type': 'application/json',
@@ -80,24 +170,71 @@ function apiGetAlongRoute(origin, destination, travelTime) {
     )
 }
 
-function renderActivity(name) {
+function renderActivity(activity, number) {
     const trip = document.querySelector("#trip");
 
     const tr = document.createElement("tr");
     trip.appendChild(tr);
     const nrTd = document.createElement("td");
-    nrTd.innerText = "1";
+    nrTd.innerText = number;
     tr.appendChild(nrTd);
     const hourTd = document.createElement("td");
-    hourTd.innerText = "1";
+    hourTd.innerText = activity.arrival + " - " + activity.departure;
     tr.appendChild(hourTd);
     const nameTd = document.createElement("td");
-    nameTd.innerText = name;
+    nameTd.innerText = activity.place.name;
     tr.appendChild(nameTd);
     const durationTd = document.createElement("td");
-    durationTd.innerText = "1";
+    durationTd.innerText = (activity.duration / DAY).toFixed(0)
+        + "d "
+        + ((activity.duration % DAY) / 60).toFixed(0)
+        + "h "
+        + activity.duration % 60
+        + "m";
     tr.appendChild(durationTd);
     const descriptionTd = document.createElement("td");
-    descriptionTd.innerText = "1";
+    descriptionTd.innerText = activity.description;
     tr.appendChild(descriptionTd);
+}
+
+function renderActivities() {
+    for (let i = 0; i < activities.length - 1; i++) {
+        const current = activities[i];
+        const next = activities[i + 1];
+
+        renderActivity(current, i * 2 + 1);
+        const travel = new Activity(
+            {name: "travel from " + current.place.name + " to " + next.place.name},
+            minutesBetweenTimes(current.departure, next.arrival),
+            current.departure,
+            next.arrival,
+            -1,
+            ""
+        );
+        renderActivity(travel, i * 2 + 2);
+    }
+    renderActivity(activities[activities.length - 1], activities.length*2-1);
+}
+
+function renderAlongRoute(places){
+    const alongRouteThere = document.querySelector("#alongThere")
+    places.forEach(place=>{
+        const tr = document.createElement("tr");
+        const nameTd = document.createElement("td");
+        nameTd.innerText = place.name;
+        tr.appendChild(nameTd);
+        const addressTd = document.createElement("td");
+        addressTd.innerText = place.address;
+        tr.appendChild(addressTd);
+        const categoriesTd = document.createElement("td");
+        categoriesTd.innerText = place.categories.map(c=>c.name).reduce((acc,curr)=>acc+" "+curr);
+        tr.appendChild(categoriesTd);
+        const addTd = document.createElement("td");
+        const addButton = document.createElement("input");
+        addTd.appendChild(addButton);
+        tr.appendChild(addTd);
+        addButton.value = "add";
+
+        alongRouteThere.appendChild(tr);
+    })
 }
--- src/main/webapp/WEB-INF/views/app/tripForm.jsp
+++ src/main/webapp/WEB-INF/views/app/tripForm.jsp
@@ -5,7 +5,7 @@
     <title>Add trip</title>
 </head>
 <body>
-<form method="post">
+<form method="post" id="tripForm">
     <div class="tab" id="basic">
         <p>Days: <input type="number" name="days"></p>
         <p>Name: <input type="text" name="name"></p>
@@ -16,16 +16,28 @@
         <input type="hidden" name="${_csrf.parameterName}" value="${_csrf.token}" id="_CSRF_"/>
         <p><input type="submit"></p>
     </div>
-    <div class="tab" id="table">
-        <table id="trip">
-            <tr>
-                <th>Nr.</th>
-                <th>Hour</th>
-                <th>Name</th>
-                <th>Duration</th>
-                <th>Description</th>
-            </tr>
-        </table>
+    <div class="tab">
+        <div>
+            <table id="trip" border="1">
+                <tr>
+                    <th>Nr.</th>
+                    <th>Hour</th>
+                    <th>Name</th>
+                    <th>Duration</th>
+                    <th>Description</th>
+                </tr>
+            </table>
+        </div>
+        <div>
+            <table id="alongThere" border="1">
+                <tr>
+                    <th>Name</th>
+                    <th>Address</th>
+                    <th>Categories</th>
+                    <th>Action</th>
+                </tr>
+            </table>
+        </div>
     </div>
 </form>
 <script src="<c:url value="/TripForm.js"/>"></script>
