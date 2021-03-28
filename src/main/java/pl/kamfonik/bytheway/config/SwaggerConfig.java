81ad5de7ff66791b5518147736a09161bba84265
MichalKamfonik <michal.kamfonik@gmail.com>
2021-03-28 21:24:44 +0200

    pom.xml: added springfox-boot-starter dependency SwaggerConfig: added

---
+++ src/main/java/pl/kamfonik/bytheway/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package pl.kamfonik.bytheway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("ByTheWayRestApi")
+                .description("Rest endpoints used by ByTheWay application")
+                .version("0.0.1")
+                .build();
+    }
+
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .build();
+    }
+}
